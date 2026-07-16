93ee64b7e87745eafb4bd2e09f181d801cf01418
mery56lores <mery56lores@outlook.com>
2026-07-16 14:36:55 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+function Header(){
+
+return (
+
+<header className="header">
+
+<h1>TxTag</h1>
+
+<p>
+Smart Expense and Receipt Management Platform
+</p>
+
+</header>
+
+);
+
+}
+
+export default Header;
